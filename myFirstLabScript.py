9951acc1ecafa53a45ddcca83da2a64075173174
Batuhan Deliaslan <153916419+batuhandeliaslan@users.noreply.github.com>
2026-02-26 13:05:52 +0300

    Update myFirstLabScript.py

--- myFirstLabScript.py
+++ myFirstLabScript.py
@@ -4,14 +4,4 @@ print("Hello",name)
 Id= input("What is your Id?")
 print("Your Id is" ,Id)"""
 
-*****************************************************************************************
 
-total_seconds = int in(input("Enter a large integer representing total seconds:  "))
-hours= total_seconds // 3600
-remaining_seconds=total_seconds % 3600 
-minutes = remaining_seconds // 60
-seconds = remaining_seconds % 60
-print (f"{total_seconds} seconds is {hours} hours,{minutes} minutes , and {seconds} seconds .")
-
-
-*****************************************************************************************
